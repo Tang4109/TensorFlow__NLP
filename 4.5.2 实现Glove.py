fd521465a3394c60f0bf4e26707be7dac68ab4ec
Tang4109 <2298964704@qq.com>
2019-12-30 17:25:52 +0800

    commit

--- 4.5.2 实现Glove.py
+++ 4.5.2 实现Glove.py
@@ -10,6 +10,7 @@ _*_coding: utf-8 _*_
 
 from __future__ import print_function
 import collections
+import csv
 import math
 import pickle
 
@@ -42,7 +43,7 @@ with open('cooc_mat.pickle', 'rb') as handle:
     cooc_mat=pickle.load(handle)
 
 vocabulary_size = 50000
-
+data_index=0
 def generate_batch(batch_size, window_size):
     # data_index is updated by 1 everytime we read a data point
     global data_index
@@ -252,5 +253,82 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
                 print(log)
 
     final_embeddings = normalized_embeddings.eval()
+
+np.save('Glove_embeddings', final_embeddings)
+
+with open('struct_skip_losses.csv', 'wt') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(final_embeddings)
+
+
+# 绘图
+def find_clustered_embeddings(embeddings, distance_threshold, sample_threshold):
+    # 计算余弦相似度
+    cosine_sim = np.dot(embeddings, np.transpose(embeddings))
+    # embeddings ** 2对每个元素进行平方
+    # axis=1 按行求和，reshape转回原来的维度
+    norm = np.dot(np.sum(embeddings ** 2, axis=1).reshape(-1, 1),
+                  np.sum(np.transpose(embeddings) ** 2, axis=0).reshape(1, -1))
+    assert cosine_sim.shape == norm.shape
+    # 应该对norm开根号才对
+    cosine_sim /= np.sqrt(norm)
+    # 将对角线元素填充为-1，排除自身
+    np.fill_diagonal(cosine_sim, -1.0)
+    argmax_cos_sim = np.argmax(cosine_sim, axis=1)
+    mod_cos_sim = cosine_sim
+    for _ in range(sample_threshold - 1):
+        # 寻找相似度最大的单词的索引
+        argmax_cos_sim = np.argmax(cosine_sim, axis=1)
+        mod_cos_sim[np.arange(mod_cos_sim.shape[0]), argmax_cos_sim] = -1
+
+    max_cosine_sim = np.max(mod_cos_sim, axis=1)
+    # 这里的意思是选出与10个以上单词余弦距离大于0.25的单词，即寻找紧密聚集的嵌入，返回单词所在行数
+    return np.where(max_cosine_sim > distance_threshold)[0]
+
+
+# 前1000个单词绘图展示
+num_points = 1000  # we will use a large sample space to build the T-SNE manifold and then prune it using cosine similarity
+tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+print('Fitting embeddings to T-SNE. This can take some time ...')
+# get the T-SNE manifold
+selected_embeddings = final_embeddings[:num_points, :]
+two_d_embeddings = tsne.fit_transform(selected_embeddings)
+
+print('Pruning the T-SNE embeddings')
+# prune the embeddings by getting ones only more than n-many sample above the similarity threshold
+# this unclutters the visualization
+selected_ids = find_clustered_embeddings(selected_embeddings, .25, 10)
+two_d_embeddings = two_d_embeddings[selected_ids, :]
+
+print('Out of ', num_points, ' samples, ', selected_ids.shape[0], ' samples were selected by pruning')
+
+
+# Plotting the t-SNE Results with Matplotlib
+def plot(embeddings, labels):
+    n_clusters = 20  # number of clusters
+    cmap = pylab.cm.get_cmap("Spectral")
+    label_colors = [cmap(float(i) / n_clusters) for i in range(n_clusters)]
+
+    assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
+
+    # Define K-Means
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', random_state=0).fit(embeddings)
+    kmeans_labels = kmeans.labels_
+
+    pylab.figure(figsize=(15, 15))  # in inches
+
+    # plot all the embeddings and their corresponding words
+    for i, (label, klabel) in enumerate(zip(labels, kmeans_labels)):
+        x, y = embeddings[i, :]
+        pylab.scatter(x, y, c=label_colors[klabel])
+
+        pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
+                       ha='right', va='bottom', fontsize=7)
+
+    pylab.show()
+
+
+words = [reverse_dictionary[i] for i in selected_ids]
+plot(two_d_embeddings, words)
 
 
